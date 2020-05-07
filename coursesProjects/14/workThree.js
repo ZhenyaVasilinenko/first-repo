97d9e35edf3a3d102021e327034672147000bd76
Zhenya <vasvasjane@gmail.com>
2020-05-07 21:01:46 +0300

    http.get

---
+++ coursesProjects/14/workThree.js
@@ -0,0 +1,16 @@
+const https = require('https');
+
+https.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
