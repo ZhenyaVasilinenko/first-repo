22394bcf24441191bee0b03d1b4e38db16de66f5
Zhenya <vasvasjane@gmail.com>
2020-06-01 18:30:19 +0300

    simple chat

---
+++ coursesProjects/19/chat/app.js
@@ -0,0 +1,31 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+let users = [];
+io.on('connection', function(socket){
+    console.log('A user connected');
+    socket.on('setUserName', function(data){
+        console.log(data);
+        if (users.indexOf(data)> - 1){
+            console.log(data);
+            socket.emit('userExists', `<p class="bg-primary"> User`  + `<b>` + data + `</b>`
+            + ` already exist, choose another name! </p>`);
+        } else {
+            users.push(data);
+            socket.emit('userSet', {userName: data});
+        }
+    });
+
+    socket.on('message', function(data){
+        io.sockets.emit('newMessage', data);
+    })
+});
+
+http.listen(3000,'172.18.73.33'||'localhost', function(){
+    console.log('Go to chat at http://localhost:3000');
+});
